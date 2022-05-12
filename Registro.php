38a85188ae9012d1789756d2f039e2b9cdd53ba7
emmuriel <emarmur108@g.educaand.es>
2022-05-12 23:41:02 +0200

    bootstrapForm

--- Registro.php
+++ Registro.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="es">
 
@@ -8,64 +9,56 @@
     <title>Registro Usuario</title>
     <link href="CSS/Formulario1.css" rel="stylesheet" type="text/css" />
     <link rel="icon" href="images/favicon.ico" type="image/png" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 
-<body>
+<body class="bg-dark" >
     
-        <div class="contenedor">
-        <form id="form1" method="POST" action="Registro.php">
-            <h1 class="cabecera">Hundir<span>Flota</span></h1>
-            <div class="contacto">
-                <div class="contact-form">
-                    <h3>Nuevo Usuario</h3>
-                    <p>
-                        <label id="lbl_email">Correo electronico: </label>
-                        <input type="email" name="email" id="txt_email">
-                    </p>
-                    <p>
-                        <span id="lbl_error_email" visible="false"></span>
-                    </p>
-
+    <div class="container min-vw-25">
+        <div class="row">
+            <div class="col">
+                <div class="mb-3">
+                    <h1 class="cabecera text-light text-center">Hundir<span>Flota</span></h1>
+                </div>
+                <form id="form1" method="POST" action="Registro.php" class="shadow-lg p-5 pt-0 pb-1 m-4 mt-1 mb-0">
+                    <h2 class="text-light text-center p-2">Nuevo Usuario</h2>
+                    <div class="mb-3">
+                        <label for="txt_email" class="form-label text-light" id="lbl_email">Correo electronico: </label>
+                        <input type="email" class="form-control bg-dark text-light border-primary" name="email" id="txt_email" aria-describedby="Introduce un email">
+                    </div>
                     <!--nombre de usuario -->
-                    <p>
-                        <label id="lbl_nombre">Nombre de usuario: </label>
-                        <input type="text" name="nombre" id="txt_nombre">
-                    </p>
-                    <p>
-                        <span id="lbl_error_nombre" visible="false"></span>
-                    </p>
-                    <!--contraseña -->
-                    <p>
-                        <label id="lbl_password">Tu contraseña: </label>
-                    </p>
-                    <input type="password" name="password" id="txt_password">
-                    <span id="lbl_error_password" visible="false"></span>
-                    </p>
-                    <!--Confirmación contraseña -->
-                    <p>
-                        <label id="lbl_conf_pass">Confirma tu contraseña:</label>
-                        <input type="password" name="confirmacion" id="txt_conf_pass">
-                        <span id="lbl_error_confpass" visible="false"></span>
-                    </p>
-
+                    <div class="mb-3">
+                        <label for="nombreUsu" class="form-label text-light">Nombre para el usuario: </label>
+                        <input type="text" class="form-control bg-dark text-light border-primary" id="nombreUsu"  name="nombre">
+                       <!-- <div class="valid-feedback">esta todo ok</div>
+                        <div class="invalid-feedback">esta un pokito mal</div> -->
+                    </div>
+                    <div>
+                        <label for="txt_password" class="form-label text-light">Tu contraseña:  </label>
+                        <input type="password" class="form-control bg-dark text-light border-primary" name="password" id="txt_password">
+                    </div>
+                        <div id="txt_password" class="form-text mb-3">
+                        La contraseña debe contener al menos un caracter numérico y una letra mayuscula-minuscula.
+                      </div>
+                    <div class="mb-3">
+                        <label for="txt_conf_pass" class="form-label text-light">Repite la contraseña: </label>
+                        <input type="password" class="form-control bg-dark text-light border-primary" name="confirmacion" id="txt_conf_pass">
+                    </div>
                     <!--boton registrar ahora-->
-                    <p>
-                        <button type="submit" name="registrar" id="registrar" value="Registrarse">Registrarse</button>
-                    </p>
-                </div>
-                <div class="contact-info">
-                    <h4>Más info</h4>
-                    <ul>
-                        <li>Escríbenos a <i>&raquo</i> hundirlaflotawm@gmail.com</li>
-                    </ul>
-                    
-                </div>
+                    <button type="submit" class="btn btn-primary" name="registrar" id="registrar" value="Registrarse">Registrarse</button>
+                 
+                    <div class="contact-info text-light mt-3 p-3">
+                        <h4>Más info</h4>
+                        <ul>
+                            <li>Escríbenos a <i>&raquo</i>hundirlaflotawm@outlook.es</li>
+                        </ul>
+                    </div>
+                </form>
             </div>
-            </form>
-        </div>
-    
+        </div> 
+    </div>
+  
 </body>
-
 </html>
 
 <?php
